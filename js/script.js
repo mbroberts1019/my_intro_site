db21c35cc796bfeab27be06e0d2af817e769aa2c
Mike Roberts <mikebroberts@hotmail.com>
2019-04-11 00:14:00 -0600

    removing irrelevant code from toggleBackground()

--- js/script.js
+++ js/script.js
@@ -3,35 +3,38 @@
 
 
 
-function iDontKnow(){
+function iDontKnow() {
   alert("Haven't decided what to do with this button yet");
 
 }
 
 var originalBackground = true;
 
-function toggleBackground(){
+function toggleBackground() {
   let bodyClass = document.getElementById("index-body");
-  console.log(bodyClass);
-  let orginalBackground = true;
-  if (!originalBackground){
+  if (!originalBackground) {
     bodyClass.style.backgroundImage = "radial-gradient(#00bdaa,#257aa6,#621e81)";
     originalBackground = true;
-  }
-  else if (originalBackground){
+  } else if (originalBackground) {
     bodyClass.style.backgroundImage = "radial-gradient(#fac70b,#127681,#df0e62)";
     originalBackground = false;
-  }else{
+  } else {
     console.log("not working yet");
   }
 }
 
-function toldYouSo(){
+function toldYouSo() {
   let dogImg = document.getElementById("cliffImg");
   dogImg.src = "images/cliffypoo.jpeg";
-  setTimeout(function(){ alert("Told you so!"); }, 1000);
-  setTimeout(function(){ dogImg.src = "images/cliffturd.jpeg"; }, 5000);
-  setTimeout(function(){ dogImg.src = "images/handsomeman.jpeg"; }, 10000);
+  setTimeout(function() {
+    alert("Told you so!");
+  }, 1000);
+  setTimeout(function() {
+    dogImg.src = "images/cliffturd.jpeg";
+  }, 5000);
+  setTimeout(function() {
+    dogImg.src = "images/handsomeman.jpeg";
+  }, 10000);
 
 
 
